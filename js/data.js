49bc03ab41d9897c3164b9e30c524acc35fd595d
liusiqian <liusiqian@yidian-inc.com>
2017-02-03 18:08:02 +0800

    修正了json格式

--- js/data.js
+++ js/data.js
@@ -20,12 +20,12 @@ var _article_json = _simpleArticleBuilder('技术','tech','Android实战——Gr
     _simpleArticleBuilder('技术','tech','从客户端的角度设计后端的接口',
     'api1','http://mp.weixin.qq.com/s/l0CbwPwUiBzNCoR6SJluZw','2017-01-05');
 
-var _feedback_json = '{error_code:0,error_msg:"success!",data:{[' + _article_json + ']}}';
+var _feedback_json = '{"error_code":0,"error_msg":"success!","data":[' + _article_json + ']}';
 var articleDatas = JSON.parse(_feedback_json);
 
 function _simpleArticleBuilder(category_name, category_id, article_title, article_id, src_url, publish_date) {
-    return '{category_name:"' + category_name + '",' + 'category_id:"' + category_id + '",' +
-        'article_title:"' + article_title + '",' + 'article_id:"' + article_id + '",' +
-        'src_url:"' + src_url + '",' + 'publish_date:"' + publish_date + '"}';
+    return '{"category_name":"' + category_name + '",' + '"category_id":"' + category_id + '",' +
+        '"article_title":"' + article_title + '",' + '"article_id":"' + article_id + '",' +
+        '"src_url":"' + src_url + '",' + '"publish_date":"' + publish_date + '"}';
 }
 
